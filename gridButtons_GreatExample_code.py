0c0b70f9e26cedaa886aafae5e66da1824cc6389
foxandrewd <fox.andrew.d@gmail.com>
2020-08-28 14:26:03 +1000

    Update gridButtons_GreatExample_code.py

--- gridButtons_GreatExample_code.py
+++ gridButtons_GreatExample_code.py
@@ -4,18 +4,17 @@ root = tk.Tk()
 # Make a 2D grid/array of buttons, inside the Tkinter main window which is called 'root' in this example
 for rowNum in range(3):
     for colNum in range(3):
-        button = tk.Button( root ,
-                            text = str(rowNum)+" "+str(colNum) ,
-                            command = lambda r=rowNum, c=colNum: printOnClick(r, c)
+        button = tk.Button( root ,                                              # This button's parent TKinter object will be the main TKinter window, here it's called 'root'
+                            text = str(rowNum)+" "+str(colNum) ,                # Button text will be "3 4" when rowNum==3 and colNum==4
+                            command = lambda r=rowNum, c=colNum: printOnClick(r, c)  # Run the printOnClick(r,c) function when button is clicked. r is row num, c is col num.
                            )
-        button.grid(row=rowNum, column=colNum)
+        button.grid(row=rowNum, column=colNum)                                  # Add this button to a grid structure in the root window, with row=rowNum & column=colNum
 
 # Show how Tkinter's grid_slaves() function works really great!!!
 def printOnClick(r, c):
-    widget = root.grid_slaves(row=r, column=c)[0]
-    #print(widget, widget['text'])
-    [button_row, button_column] = widget['text'].split(' ')
-    print("Button Clicked was: Row = " + button_row + ", Column = " + button_column)
+    widget = root.grid_slaves(row=r, column=c)[0]                           # Use TKinter's grid_slaves function to get the Button at pos (r,c) and store it in var 'widget'
+    [button_row, button_column] = widget['text'].split(' ')                 # Retrieve the button's row and column numbers from 'widget's' 'text' element
+    print("Button Clicked was: Row = " + button_row + ", Column = " + button_column)    # Print out the Row and Column numbers of the button that got clicked on
 
 # Run the TKinter Main GUI Program, show it on the screen:
 tk.mainloop()
