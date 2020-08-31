cf90e21fe5310cfb42e3e7b47c476bc4619c5e02
foxandrewd <fox.andrew.d@gmail.com>
2020-08-31 12:42:21 +1000

    MAJOR UPDATE!!!!!
    changed how buttons and there positions are stored.
    not stored in the button text anymore, but the buttons
    are stored in a List, and the button positions are stored
    in a Dict.

--- gridButtons_GreatExample_code.py
+++ gridButtons_GreatExample_code.py
@@ -1,22 +1,30 @@
-import tkinter as tk
-root = tk.Tk()
+import tkinter as tk  # Import the TKinter library/module so we can use it
+DEBUG = 3
+root = tk.Tk()  # Create the top-level Window object. It's not displayed yet, though!
 
-# Make a 2D grid/array of buttons, inside the Tkinter main window which is called 'root' in this example
-for rowNum in range(3):                 # rowNum will take on the value 0, then 1, then 2 ( i.e. in [0,1,2] )
-    for colNum in range(3):             # colNum will take on the value 0, then 1, then 2 ( i.e. in [0,1,2] )
-        button = tk.Button(root,                                              # This button's parent TKinter object will be the main TKinter window, here it's called 'root'
-                           text = str(rowNum)+" "+str(colNum) ,                # Button text will be "3 4" when rowNum==3 and colNum==4
-                           command = lambda r=rowNum, c=colNum: printOnClick(r, c)  # Run the printOnClick(r,c) function when button is clicked. r is row num, c is col num.
-                           )
-        button.grid(row=rowNum, column=colNum)                                  # Add this button to a grid structure in the root window, with row=rowNum & column=colNum
+buttons = { 0:{0:None,1:None,2:None}, 1:{0:None,1:None,2:None}, 2:{0:None,1:None,2:None} }
+button_pos = {}
+
+# Make a 2D array of buttons
+for rowN in range(3):
+    for colN in range(3):
+        button = tk.Button(root, text = "", height=3, width=6,
+                           command = lambda r=rowN, c=colN: doStuffOnClick(r, c) )
+        buttons[rowN][colN] = button            # Save this new button into the "buttons" List variable
+        button_pos[button] = (rowN, colN)       # Save the position of this button into the "button_pos" Dict
+        if DEBUG >= 2:
+            print(button , button_pos[button])      # 
+        button.grid(row=rowN, column=colN)      # Place this new button into the current TKinter grid layout
     #endfor colNum
 #endfor rowNum
 
-# Show how Tkinter's grid_slaves() function works well
-def printOnClick(r, c):
-    widget = root.grid_slaves(row=r, column=c)[0]                           # Use TKinter's grid_slaves function to get the Button at pos (r,c) and store it in var 'widget'
-    [button_row, button_column] = widget['text'].split(' ')                 # Retrieve the button's row and column numbers from 'widget's' 'text' element
-    print("Button Clicked was: Row = " + button_row + ", Column = " + button_column)    # Print out the Row and Column numbers of the button that got clicked on
+# Print out some helpful information when we click on one of the buttons in the grid
+def doStuffOnClick(r, c):
+    theButton = buttons[r][c]
+    print("The button name/ID is: " + str(theButton) )
+    theButtonPos = button_pos[theButton]
+    (button_row, button_column) = button_pos[theButton]
+    print("Button Clicked was: Row=" + str(button_row) + \
+          ", Column=" + str(button_column) )                        # Backslash ('\') lets you continue ur code statement on the next line
 
-# Run the TKinter Main GUI Program, show it on the screen:
-tk.mainloop()
+tk.mainloop()   # Run the TKinter Main GUI Program, i.e. show it on the screen.
