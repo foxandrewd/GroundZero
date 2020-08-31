4f0db252d58f640aa719d76123c3af924cdbf2af
foxandrewd <fox.andrew.d@gmail.com>
2020-08-31 10:18:53 +1000

    Update gridButtons_GreatExample_code.py

--- gridButtons_GreatExample_code.py
+++ gridButtons_GreatExample_code.py
@@ -4,11 +4,13 @@ root = tk.Tk()
 # Make a 2D grid/array of buttons, inside the Tkinter main window which is called 'root' in this example
 for rowNum in range(3):                 # rowNum will take on the value 0, then 1, then 2 ( i.e. in [0,1,2] )
     for colNum in range(3):             # colNum will take on the value 0, then 1, then 2 ( i.e. in [0,1,2] )
-        button = tk.Button( root ,                                              # This button's parent TKinter object will be the main TKinter window, here it's called 'root'
-                            text = str(rowNum)+" "+str(colNum) ,                # Button text will be "3 4" when rowNum==3 and colNum==4
-                            command = lambda r=rowNum, c=colNum: printOnClick(r, c)  # Run the printOnClick(r,c) function when button is clicked. r is row num, c is col num.
+        button = tk.Button(root,                                              # This button's parent TKinter object will be the main TKinter window, here it's called 'root'
+                           text = str(rowNum)+" "+str(colNum) ,                # Button text will be "3 4" when rowNum==3 and colNum==4
+                           command = lambda r=rowNum, c=colNum: printOnClick(r, c)  # Run the printOnClick(r,c) function when button is clicked. r is row num, c is col num.
                            )
         button.grid(row=rowNum, column=colNum)                                  # Add this button to a grid structure in the root window, with row=rowNum & column=colNum
+    #endfor colNum
+#endfor rowNum
 
 # Show how Tkinter's grid_slaves() function works well
 def printOnClick(r, c):
